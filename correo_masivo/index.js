610bd4a2284b7a2d756204b254c4990481317f7e
Duvier SENA <duvierdiegodavid.0715@gmail>
2025-06-03 06:42:53 -0500

    Format document

--- correo_masivo/index.js
+++ correo_masivo/index.js
@@ -1,16 +1,19 @@
-// index.js
 const enviarCorreoMasivo = require('./mailer');
 
+//Agregamos la lista de correos que necesitemos
 const listaCorreos = [
     "correo1@ejemplo.com",
     "correo2@ejemplo.com",
+    "correo3@ejemplo.com",
+    "correo4@ejemplo.com",
 ];
 
+//Estuctura del correo enviado
 const asunto = "🎉 ¡Promoción especial!";
 const contenido = `
     <h1>Hola 👋</h1>
     <p>Gracias por participar en nuestra campaña.</p>
-    <p>xoxoxo.</p>
+    <p>Te agradecemos el apoyo que nos brindan <3.</p>
 `;
 
 enviarCorreoMasivo(listaCorreos, asunto, contenido);
